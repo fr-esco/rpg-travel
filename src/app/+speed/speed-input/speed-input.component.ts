d889845c889a60074b9a7cda0736981e85a88bbe
Francesco Colamonici <f.colamonici@gmail.com>
2016-06-01 17:09:19 +0200

    stream rewrite

--- src/app/+speed/speed-input/speed-input.component.ts
+++ src/app/+speed/speed-input/speed-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Control } from '@angular/common';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
 import { MD_RADIO_DIRECTIVES, MdRadioDispatcher } from '@angular2-material/radio';
@@ -6,8 +7,10 @@ import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
 import 'js-quantities';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
+import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/merge';
 
 const SOM_METRIC = 'metric';
 const SOM_IMPERIAL = 'imperial';
@@ -36,16 +39,24 @@ interface Speed {
     MdRadioDispatcher
   ]
 })
-export class SpeedInputComponent implements OnInit {
+export class SpeedInputComponent implements OnDestroy, OnInit {
   @Input('system') som: string;
   @Output('select') onSelect = new EventEmitter<QtyModule.Qty>();
+
+  speedSubscription: Subscription;
 
   speedUnits: string[];
   speed: Speed;
 
-  speedStream = new Subject<Speed>();
+  scalar = new Control();
+  unit = new Control();
 
   constructor() { }
+
+  ngOnDestroy() {
+    if (this.speedSubscription)
+      this.speedSubscription.unsubscribe();
+  }
 
   ngOnInit() {
     this.speedUnits = UOM[this.som || SOM_METRIC] || UOM[SOM_METRIC];
@@ -53,36 +64,34 @@ export class SpeedInputComponent implements OnInit {
       scalar: 9,
       unit: this.speedUnits[1]
     };
-
-    this.speedStream
-      .debounceTime(3000)
-      .distinctUntilChanged((x: Speed, y: Speed) => x.scalar === y.scalar && x.unit === y.unit)
-      .subscribe((s: Speed) => this.emitSpeed(s));
+    this.speedSubscription = this.initStreams();
   }
 
-  buffer() {
-    console.log('buffer', this.speed);
-    this.speedStream.next(Object.assign({}, this.speed));
-  }
-
-  speedUnit(unit: string) {
-    console.log('speedUnit', unit);
-    this.speed.unit = unit;
-    this.buffer();
-  }
-
-  speedScalar(scalar: number) {
-    console.log('speedScalar', scalar);
-    this.speed.scalar = scalar;
-    this.buffer();
-  }
-
-  emitSpeed(s: Speed) {
+  private emitSpeed(s: Speed) {
     if (s.unit.indexOf('round') > -1) {
       s.scalar = s.scalar * 6;
       s.unit = s.unit.replace('round', 's');
     }
 
     this.onSelect.emit(Qty(s.scalar, s.unit));
+  }
+
+  private initStreams() {
+    let scalarStream = this.scalar.valueChanges
+      .debounceTime(400)
+      // .distinctUntilChanged()
+      .do(value => console.log('scalar', value))
+      .map(value => this.speed.scalar = value);
+
+    let unitStream = this.unit.valueChanges
+      .debounceTime(400)
+      .distinctUntilChanged()
+      .do(value => console.log('unit', value))
+      .map(value => this.speed.unit = value);
+
+    return scalarStream.merge(unitStream)
+      .debounceTime(200)
+      .map(() => Object.assign({}, this.speed))
+      .subscribe((s: Speed) => this.emitSpeed(s));
   }
 }
